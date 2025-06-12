6d38db265114592dd31b04f20f5831cdbcfb6e28
Aizaz Ullah Durrani <noobplayer6335@gmail.com>
2025-06-12 23:34:23 +0500

    A program to check if a given phrase contains every letter of the English alphabet at least once (i.e., is a pangram).

---
+++ pangram_check.py
@@ -0,0 +1,8 @@
+def pangram(phrase):
+    letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+
+    phrase = set(phrase.lower())  # convert to lowercase for case insensitivity
+
+    return phrase >= letters
+
+print(pangram('the quick brown fox jumps over the lazy'))
