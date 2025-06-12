e24d921207ba6a05af088ad4e6fdd33acd7c67f6
Aizaz Ullah Durrani <noobplayer6335@gmail.com>
2025-06-12 23:37:15 +0500

    A program to count the number of lowercase and uppercase letters in a given string.

---
+++ count_case.py
@@ -0,0 +1,12 @@
+def count(phrase):
+    lower = 0
+    upper = 0
+
+    for l in phrase:
+        if l.islower():
+            lower += 1
+        elif l.isupper():
+            upper += 1
+    return lower, upper
+
+print(count('ABCI'))
