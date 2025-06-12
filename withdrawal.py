352d8a3d373900e31ada29c6b4d517a10c9b1a18
Aizaz Ullah Durrani <noobplayer6335@gmail.com>
2025-06-12 23:55:58 +0500

    A program to simulate withdrawal from an account enforcing a minimum balance limit using a custom exception.

---
+++ withdrawal.py
@@ -0,0 +1,18 @@
+class AccountBalanceException(Exception):
+    pass
+
+balance = 10000
+
+def withdraw(amt):
+    global balance
+
+    if balance - amt < 5000:
+        raise AccountBalanceException('Minimum Balance should be 5000')
+    else:
+        balance = balance - amt
+        print('Remaining balance is', balance)
+
+try:
+    withdraw(6000)
+except AccountBalanceException as e:
+    print(e)
