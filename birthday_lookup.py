1c6eea565a75a208d303326aebd4247c941e284e
Aizaz Ullah Durrani <noobplayer6335@gmail.com>
2025-06-12 23:17:17 +0500

    A Python program to look up a person's birthday by name using a dictionary.

---
+++ birthday_lookup.py
@@ -0,0 +1,14 @@
+birthdays = {
+    'Sachin': '03/14/2003',
+    'Carl': '01/17/2001',
+    'Khan': '12/10/2006',
+    'Donald': '06/14/2010',
+    'Rohan': '01/06/2005'
+}
+
+name = input('Enter Name: ')
+
+if name in birthdays:
+    print('Mr./Miss {} was born on {}'.format(name, birthdays[name]))
+else:
+    print('Name is not found')
