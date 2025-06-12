d46602c5a66d8fc174f0fe7b5de835bad2b91b98
Aizaz Ullah Durrani <noobplayer6335@gmail.com>
2025-06-13 00:30:19 +0500

    Implements a Rational class to perform addition and subtraction on rational numbers using operator overloading.

---
+++ rational_number_operations.py
@@ -0,0 +1,25 @@
+class Rational:
+
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+
+    def __add__(self, other):
+        p = self.p * other.q + self.q * other.p
+        q = self.q * other.q
+        return Rational(p, q)
+
+    def __sub__(self, other):
+        p = self.p * other.q - self.q * other.p
+        q = self.q * other.q
+        return Rational(p, q)
+
+    def __str__(self):
+        return str(self.p) + '/' + str(self.q)
+
+
+r1 = Rational(2, 3)
+r2 = Rational(1, 2)
+r3 = r1 + r2
+
+print(r1, '+', r2, '=', r3)
